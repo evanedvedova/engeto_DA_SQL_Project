d00309a86ebf787e9ba3684bc835c139fdb40644
evanedvedova <evanedvedova@email.cz>
2025-11-20 10:36:53 +0100

    Adding SQL files

---
+++ 20251118  question 1.sql
@@ -0,0 +1,9 @@
+SELECT
+    industry_branch,
+    round(regr_slope(payroll_value, record_year + (record_quarter -1)*0.25)) AS slope,
+    round(regr_intercept(payroll_value, record_year + (record_quarter  -1)*0.25)) AS intercept,
+    round(regr_r2(payroll_value, record_year + (record_quarter -1)*0.25)::numeric,2) AS r2
+FROM t_eva_nedvedova_project_sql_primary_final 
+WHERE industry_branch IS NOT null
+GROUP BY industry_branch 
+ORDER BY trend_sklon DESC;
