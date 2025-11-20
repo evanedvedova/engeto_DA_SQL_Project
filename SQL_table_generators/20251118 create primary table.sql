4419a0611542711a585d17367714e5bb8537ec9d
evanedvedova <evanedvedova@email.cz>
2025-11-20 10:46:12 +0100

    Add files via upload

---
+++ SQL_table_generators/20251118 create primary table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE t_eva_nedvedova_project_SQL_primary_final (
+    id SERIAL PRIMARY KEY,
+
+    -- for all records
+    record_type VARCHAR(20),  -- 'wages' nebo 'prices'
+    record_year INT,
+    record_quarter INT,
+
+    -- for wages
+    payroll_value NUMERIC(12,2),  
+    industry_branch VARCHAR(255),
+    industry_branch_code VARCHAR(255),
+    value_type_code INT,
+    calculation_code INT,
+
+    -- for prices
+    category VARCHAR(255),
+    price_value NUMERIC(10,1),
+    category_code INT
+);
