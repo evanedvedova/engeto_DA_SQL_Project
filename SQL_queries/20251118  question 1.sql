f194385f7aa3dd4e05d4ce9537f766f1be17ca9c
evanedvedova <evanedvedova@email.cz>
2025-11-24 13:45:37 +0100

    Refactor SQL query formatting for clarity

--- SQL_queries/20251118  question 1.sql
+++ SQL_queries/20251118  question 1.sql
@@ -3,7 +3,12 @@ SELECT
     round(regr_slope(payroll_value, record_year + (record_quarter -1)*0.25)) AS slope,
     round(regr_intercept(payroll_value, record_year + (record_quarter  -1)*0.25)) AS intercept,
     round(regr_r2(payroll_value, record_year + (record_quarter -1)*0.25)::numeric,2) AS r2
-FROM t_eva_nedvedova_project_sql_primary_final 
-WHERE industry_branch IS NOT null
-GROUP BY industry_branch 
-ORDER BY slope DESC;
+FROM 
+    t_eva_nedvedova_project_sql_primary_final 
+WHERE 
+    industry_branch IS NOT null
+GROUP BY 
+    industry_branch 
+ORDER BY 
+    slope DESC;
+
