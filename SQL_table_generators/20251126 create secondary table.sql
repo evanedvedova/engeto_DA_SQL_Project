797958f32094039075e999909b691fe18a1234a9
evanedvedova <evanedvedova@email.cz>
2025-11-26 09:06:43 +0100

    Create secondary table for project SQL

--- SQL_table_generators/20251126 create secondary table.sql
+++ SQL_table_generators/20251126 create secondary table.sql
@@ -1,7 +1,9 @@
-CREATE TABLE t_eva_nedvedova_project_sql_secondary_final (
+CREATE TABLE 
+    t_eva_nedvedova_project_sql_secondary_final (
     country varchar(50),
     record_year int,
     gdp float(20),
     population float(20),
     gini float(8)
+
 );
